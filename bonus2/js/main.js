ba39a5d81d6b699e9007aafcd7856a2ffabaddf6
ErikBorgogno <erikborgo.20@gmail.com>
2022-07-06 17:30:08 +0200

    done bonus2

--- bonus2/js/main.js
+++ bonus2/js/main.js
@@ -1,16 +1,36 @@
+let i;
+const container = document.getElementById('container');
+
 
 for (let i = 1; i <= 100; i++){ // VOGLIO AVERE UN CONTEGGIO DA 1 A 100
 
     if(i%3 == 0 && i%5 == 0){    //VOGLIO I NUMERI MULTIPLI DI 3 E 5 ABBIANO VALORE "FizzBuzz" 
-        console.log('FizzBuzz');
+        const fizzbuzz = `<div class="square fizzbuzz">FizzBuzz</div>`;
+        container.innerHTML += fizzbuzz; 
     } else if(i%3 == 0){    //VOGLIO I NUMERI MULTIPLI DI 3 ABBIANO VALORE "Fizz"
-        console.log('Fizz')
+        const fizz = `<div class="square fizz">Fizz</div>`;
+        container.innerHTML += fizz;
     }else if(i%5 == 0){ //VOGLIO I NUMERI MULTIPLI DI 5 ABBIANO VALORE "Buzz"
-        console.log('Buzz')
+        const buzz = `<div class="square buzz">Buzz</div>`;
+        container.innerHTML += buzz;
     }else{
-        console.log(i)
+        const none = `<div class="square none">${i}</div>`;
+        container.innerHTML += none;
     }
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
