5ae9f37fe4b5742f2a5c5cf6b50a3e344b98e975
ErikBorgogno <erikborgo.20@gmail.com>
2022-07-06 16:19:07 +0200

    add comments

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
 
-for (let i = 1; i <= 100; i++){
-    
-    if(i%3 == 0 && i%5 == 0){
+for (let i = 1; i <= 100; i++){ // VOGLIO AVERE UN CONTEGGIO DA 1 A 100
+
+    if(i%3 == 0 && i%5 == 0){    //VOGLIO I NUMERI MULTIPLI DI 3 E 5 ABBIANO VALORE "FizzBuzz" 
         console.log('FizzBuzz');
-    } else if(i%3 == 0){
+    } else if(i%3 == 0){    //VOGLIO I NUMERI MULTIPLI DI 3 ABBIANO VALORE "Fizz"
         console.log('Fizz')
-    }else if(i%5 == 0){
+    }else if(i%5 == 0){ //VOGLIO I NUMERI MULTIPLI DI 5 ABBIANO VALORE "Buzz"
         console.log('Buzz')
     }else{
         console.log(i)
